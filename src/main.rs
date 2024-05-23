8d93b32f5d8b4721e2625a157a4f390f3bd45e42
HitarthGandhi <hitarthgandhi01@gmail.com>
2024-05-23 01:07:40 -0700

    add timestamp function to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,41 @@
+use chrono::{Local, TimeZone};
 use clap::Parser;
 use dirs::home_dir;
+use regex::escape;
+use regex::Regex;
 use std::cmp::min;
 use std::env;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 use std::path::PathBuf;
-use regex::Regex;
-use regex::escape;
 
 /// Simple program to greet a person
 #[derive(Parser)]
-#[command(name = "greeter")]
+#[command(name = "zshhistory")]
 #[command(version = "1.0")]
-#[command(about = "Says hello to a person", long_about = None)]
+#[command(about = "Command line history tool", long_about = None)]
 struct Cli {
     // number of lines to read
     #[arg(short, long, default_value = "5")]
     lines: usize,
 
     // string to search
-    #[arg(short, long)]
-    search: Option<String>
+    #[arg(short, long, default_value = "")]
+    search: Option<String>,
 
+    // take history upto a day
+    #[arg(long, default_value = "false")]
+    day: bool,
+
+    // take history upto a month
+    #[arg(long, default_value = "false")]
+    month: bool,
 }
 
-struct HistoryItem{
-    command: String,
-    timestamp: i64
-}
+// struct HistoryItem {
+//     command: String,
+//     timestamp: i64,
+// }
 
 fn main() -> io::Result<()> {
     let args: Cli = Cli::parse();
@@ -48,16 +56,49 @@ fn main() -> io::Result<()> {
     let mut line_number = 1;
     let mut lines: Vec<String> = reader
         .lines()
-        .filter_map(|line: Result<String, io::Error>| match line {
+        .filter_map(|line_result| match line_result {
             Ok(bytes) => {
                 line_number += 1;
-                let byte_string_literal: &[u8] = bytes.as_bytes();
-                let line = String::from_utf8_lossy(byte_string_literal).into_owned();
-                let escaped_s = escape(args.search.as_ref().unwrap().as_str());
+                let line = String::from_utf8_lossy(bytes.as_bytes()).into_owned();
+                let escaped_s = match args.search.as_ref() {
+                    Some(s) => escape(s.as_str()),
+                    None => {
+                        eprintln!("Error: search argument is missing");
+                        return None;
+                    }
+                };
                 let pattern = format!("\\b{}\\b", escaped_s);
-                let regex = Regex::new(&pattern).unwrap();
+                let regex = match Regex::new(&pattern) {
+                    Ok(re) => re,
+                    Err(e) => {
+                        eprintln!("Error: Invalid regex pattern: {}", e);
+                        return None;
+                    }
+                };
 
-                if let Some(..) = regex.captures(&line){
+                if let Some(..) = regex.captures(&line) {
+                    let parts: Vec<&str> = line.split(':').collect();
+                    if let Some(timestamp_str) = parts.get(1) {
+                        // Parse the timestamp string to an integer
+                        if let Ok(timestamp) = timestamp_str.trim().parse::<i64>() {
+                            if (args.day && is_within_one_day(timestamp))
+                                || (args.month && is_within_one_day(timestamp))
+                            {
+                                println!("Timestamp: {}", timestamp);
+                                return Some(line);
+                            } else if args.month && is_within_one_month(timestamp) {
+                                println!("Timestamp: {}", timestamp);
+                                return Some(line);
+                            } else if !args.day && !args.month {
+                                return Some(line);
+                            }
+                        } else {
+                            eprintln!("Error: Unable to parse timestamp");
+                        }
+                    } else {
+                        eprintln!("Error: Timestamp not found in the input string");
+                    }
+
                     Some(line)
                 } else {
                     None
@@ -101,4 +142,19 @@ fn get_history_file_path() -> Option<std::path::PathBuf> {
         println!("Unsupported shell: {}", shell);
         None
     }
+}
+
+// Check if the timestamp is within a day
+fn is_within_one_day(timestamp: i64) -> bool {
+    let current_time = Local::now().timestamp();
+    let one_day_ago = current_time - 24 * 3600;
+    timestamp >= one_day_ago
+}
+
+// Check if the timestamp is within a month
+fn is_within_one_month(timestamp: i64) -> bool {
+    let current_time = Local::now().timestamp();
+    let one_month_ago = current_time - 30 * 24 * 3600;
+    let one_month_from_now = current_time + 30 * 24 * 3600;
+    timestamp >= one_month_ago && timestamp <= one_month_from_now
 }
