ef2c8769519a34967524f4fa6c4d9f60ad6312bf
Aditya Karad <ankarad@uci.edu>
2024-05-25 11:47:37 -0700

    removed unnecessary import

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,6 @@ use std::env;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 use std::path::PathBuf;
-use std::string;
-use std::process::Command;
 use clipboard::{ClipboardContext, ClipboardProvider};
 
 
